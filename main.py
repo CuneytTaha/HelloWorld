3c7d48f110e29562d3b2bf7fb38f9b4cff9055b8
cuneyt <cuneyt@3bfab2021>
2021-10-01 09:42:24 +0300

    change

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from threading import Thread
 from time import sleep
+
+
 def threadControl():
     counter = 0
     while True:
         counter += 1
         print(f"Thread {counter} second")
         sleep(1)
+
+
 controlThread = Thread(target=threadControl)
 controlThread.daemon = True
 controlThread.start()
